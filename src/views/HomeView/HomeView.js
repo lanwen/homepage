f348fb5b5a61260b975bb82ff68dea582969ca0f
Johannes Schickling <schickling.j@gmail.com>
2016-02-22 16:31:49 +0000

    fix tests

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classes from './HomeView.scss'
+import logoImage from './logo.jpg'
 
 export class HomeView extends React.Component {
   render () {
     return (
       <div className={classes.root}>
+        <img className={classes.duck}
+          src={logoImage}
+          alt='This is a duck, because Redux.' />
+        <h1>Flexible GraphQL backend as a service</h1>
       </div>
     )
   }
