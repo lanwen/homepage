82e02fb2135c5b0d7ab50a30f8e95627fddf1e11
Johannes Schickling <schickling.j@gmail.com>
2016-04-21 17:36:04 +0200

    react

---
+++ src/components/App/App.js
@@ -0,0 +1,256 @@
+import React from 'react'
+// import Icon from 'components/Icon/Icon'
+// import classes from './App.scss'
+
+export class SideNav extends React.Component {
+
+  render () {
+    return (
+      <div id='container'>
+        <div id='header'>
+          <div id='header-logo'>
+            <a href='http://graph.cool/'><img src='logo with lettering.svg' height='40px' alt='' /></a>
+          </div>
+          <div id='header-signin'>
+            <a id='header-signin-text' href='http://graph.cool/signin/'>Sign In</a>
+          </div>
+          <div id='header-links'>
+            <li><a href='http://graph.cool/blog/'>Blog</a></li>
+            <li><a href='http://graph.cool/docs/'>Docs</a></li>
+            <li><a href='http://graph.cool/pricing/'>Pricing</a></li>
+            <li><a href='http://graph.cool/about/'>About</a></li>
+          </div>
+        </div>
+        <div id='login1'>
+          <div id='log1text'>
+            <p>Getting started is easy. Be up and running in minutes.</p>
+          </div>
+          <div id='log1button'>
+            <div id='log1buttontext'>
+              <p>CREATE A FREE ACCOUNT </p>
+            </div>
+          </div>
+        </div>
+        <div id='features'>
+          <div id='featuretitle1'>
+            <p>Product Features</p>
+          </div>
+          <div id='featuretitle2'>
+            <p>Enjoy all the bells and whistles.</p>
+          </div>
+          <div id='fline1'>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f1.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>Flexible Data Layer</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f2.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>Based on GraphQL</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f3.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>Already familiar</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+          </div>
+          <div id='fline2'>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f4.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>Timesaver</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f5.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>No Maintenance</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+            <div className='fea'>
+              <div className='fsvg'>
+                <img src='f6.svg' height='80px' alt='' />
+              </div>
+              <div className='ftitle'>
+                <p>Security</p>
+              </div>
+              <div className='fdes'>
+                Lorem ipsum dolor sit amet, consetetur <br />
+                sadipscing elitr, sed diam nonumy eirmod <br />
+                tempor invidunt ut labore et dolore magna <br />
+                aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id='howitworks'>
+          <div id='howtitle1'>
+            <p>How it works</p>
+          </div>
+          <div id='howtitle2'>
+            <p>Setup a backend in less than 5 minutes – no coding required</p>
+          </div>
+          <div id='how1'>
+            <div id='how1svg'>
+              <img src='how1.svg' width='400px' alt='' />
+            </div>
+            <div id='how1text'>
+              <div id='how1title'>
+                <p>Define your data model</p>
+              </div>
+              <div id='how1des'>
+                <p>
+                  Define the structure of your data using <br />
+                  our intuitive model editor. You can easily <br />
+                  manage relationships between models <br />
+                  and setup access permissions.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div id='how2'>
+            <div id='how2svg'>
+              <img src='how1.svg' width='400px' alt='' />
+            </div>
+            <div id='how2text'>
+              <div id='how2title'>
+                <p>Define your data model</p>
+              </div>
+              <div id='how2des'>
+                <p>
+                  Define the structure of your data using <br />
+                  our intuitive model editor. You can easily <br />
+                  manage relationships between models <br />
+                  and setup access permissions.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div id='how3'>
+            <div id='how3svg'>
+              <img src='how1.svg' width='400px' alt='' />
+            </div>
+            <div id='how3text'>
+              <div id='how3title'>
+                <p>Define your data model</p>
+              </div>
+              <div id='how3des'>
+                <p>
+                  Define the structure of your data using <br />
+                  our intuitive model editor. You can easily <br />
+                  manage relationships between models <br />
+                  and setup access permissions.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id='pricing'>
+          <div id='box'>
+            <div className='border'></div>
+            <div id='box1text'>
+              <img id='timg'src='development.svg' height='60px' alt='' />
+              <p id='t1'>DEVELOPMENT</p>
+              <h1 id='t2'>$0</h1>
+              <p id='t3'>/MONTH</p>
+              <p id='t4'>Feature001 <br />
+                Feature002 <br />
+                Feature003 <br />
+                Feature004 <br />
+              </p>
+              <p id='t5'>READ MORE</p>
+            </div>
+          </div>
+          <div id='box'>
+            <div className='border'></div>
+            <div id='box1text'>
+              <img id='timg'src='startup.svg' height='60px' alt='' />
+              <p id='t1'>STARTUP</p>
+              <h1 id='t2'>$29</h1>
+              <p id='t3'>/MONTH</p>
+              <p id='t4'>Feature001 <br />
+                Feature002 <br />
+                Feature003 <br />
+                Feature004 <br />
+              </p>
+              <p id='t5'>READ MORE</p>
+            </div>
+          </div>
+          <div id='box'>
+            <div className='border'></div>
+            <div id='box1text'>
+              <img id='timg'src='production.svg' height='60px' alt='' />
+              <p id='t1'>DEVELOPMENT</p>
+              <h1 id='t2'>$249</h1>
+              <p id='t3'>/MONTH</p>
+              <p id='t4'>Feature001 <br />
+                Feature002 <br />
+                Feature003 <br />
+                Feature004 <br />
+              </p>
+              <p id='t5'>READ MORE</p>
+            </div>
+          </div>
+          <div id='box'>
+            <div className='border'></div>
+            <div id='box1text'>
+              <img id='timg'src='enterprise.svg' height='60px' alt='' />
+              <p id='t1'>DEVELOPMENT</p>
+              <h1 id='t6'>CONTACT <br /> US</h1>
+              <p id='t7'>Feature001 <br />
+                Feature002 <br />
+                Feature003 <br />
+                Feature004 <br />
+              </p>
+              <p id='t8'>READ MORE</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const cssnano = require('cssnano')
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    publicPath: '/'
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: 'eslint',
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /\.scss/,
+      loader: 'style!css?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap',
+      exclude: /node_modules/
+    }, {
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/
+    }, {
+      test: /icons\/.*\.svg$/,
+      loader: 'svg-inline?removeTags=true'
+    }, {
+      test: /graphics\/.*\.svg$/,
+      loader: 'file'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: 'static/favicon.png',
+      template: 'src/index.html'
+    })
+  ],
+  postcss: [
+    cssnano({
+      autoprefixer: {
+        add: true,
+        remove: true,
+        browsers: ['last 2 versions']
+      },
+      discardComments: {
+        removeAll: true
+      },
+      safe: true,
+      sourcemap: true
+    })
+  ],
+  resolve: {
+    root: [path.resolve('./src'), path.resolve('node_modules')],
+    extensions: ['', '.js']
+  }
+}
