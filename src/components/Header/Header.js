852e9bc6805d20a60e1971bf91cbf96c2412d222
Fabian Kozdon <info@fabiankozdon.com>
2016-04-25 12:04:24 +0200

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,21 +6,21 @@ export default class Header extends React.Component {
   render () {
     return (
       <div className={classes.header}>
-        <div className={classes.headerLogo}>
-          <a href='http://graph.cool/'>
-            <img height={40} src={require('assets/graphics/logo-text.svg')} />
-          </a>
+        <div className={classes.spacer}></div>
+        <a className={classes.headerLogo} href='http://graph.cool/'>
+          <img height={40} src={require('assets/graphics/logo-text.svg')} />
+        </a>
+        <div className={classes.headerLinks}>
+          <a href='http://graph.cool/blog/'>Blog</a>
+          <a href='http://graph.cool/docs/'>Docs</a>
+          <a href='http://graph.cool/pricing/'>Pricing</a>
+          <a href='http://graph.cool/about/'>About</a>
         </div>
         <div className={classes.headerSignin}>
           <a className={classes.headerSigninText} href='http://graph.cool/signin/'>Sign In</a>
         </div>
-        <div className={classes.headerLinks}>
-          <li><a href='http://graph.cool/blog/'>Blog</a></li>
-          <li><a href='http://graph.cool/docs/'>Docs</a></li>
-          <li><a href='http://graph.cool/pricing/'>Pricing</a></li>
-          <li><a href='http://graph.cool/about/'>About</a></li>
-        </div>
-      </div>
+        <div className={classes.spacer}></div>
+    </div>
     )
   }
 }
