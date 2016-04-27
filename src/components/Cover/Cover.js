ea9e471936ce5be5741527850d21161584a2f4a2
Fabian Kozdon <info@fabiankozdon.com>
2016-04-27 11:40:18 +0200

    cover

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -6,6 +6,9 @@ export default class Cover extends React.Component {
   render () {
     return (
       <div className={classes.root}>
+        <div className={classes.img}>
+          <img height={600} src={require('assets/graphics/Cover.png')} />
+        </div>
       </div>
     )
   }
