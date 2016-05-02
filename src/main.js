326b3a40c46c61d5f00ade8167e7b6a8fe4179c6
Johannes Schickling <schickling.j@gmail.com>
2016-05-02 23:16:27 +0200

    fixed merge

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< HEAD
 import {
   Router,
   Route,
   browserHistory,
 } from 'react-router'
 import App from 'components/App/App'
-=======
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { useRouterHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import makeRoutes from './routes'
-import Root from './containers/Root'
-import configureStore from './redux/configureStore'
->>>>>>> master
 import Smooch from 'smooch'
 
 ReactDOM.render(
