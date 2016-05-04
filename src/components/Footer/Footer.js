73803fc4dbd9b8ed3139b8e532f26ae08fb973c2
Johannes Schickling <schickling.j@gmail.com>
2016-05-04 19:29:38 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import classes from './Footer.scss'
 
 export default class Footer extends React.Component {
@@ -6,6 +7,43 @@ export default class Footer extends React.Component {
   render () {
     return (
       <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <div className={classes.status}>
+            <h4>Online Status</h4>
+            <div className={classes.check}>
+              <i /> Relay Endpoint
+            </div>
+            <div className={classes.check}>
+              <i /> Simple GraphQL Endpoint
+            </div>
+          </div>
+          <div className={classes.lists}>
+            <div className={classes.list}>
+              <h4>Product</h4>
+              <ul>
+                <li><Link to='https://blog.graph.cool'>Pricing</Link></li>
+                <li><Link to='https://blog.graph.cool'>Documentation</Link></li>
+                <li><Link to='https://blog.graph.cool'>Update Notes</Link></li>
+              </ul>
+            </div>
+            <div className={classes.list}>
+              <h4>Company</h4>
+              <ul>
+                <li><Link to='https://blog.graph.cool'>About</Link></li>
+                <li><Link to='https://blog.graph.cool'>Blog</Link></li>
+                <li><Link to='https://blog.graph.cool'>Documentation</Link></li>
+              </ul>
+            </div>
+            <div className={classes.list}>
+              <h4>Social</h4>
+              <ul>
+                <li><Link to='https://blog.graph.cool'>Twitter</Link></li>
+                <li><Link to='https://blog.graph.cool'>Facebook</Link></li>
+                <li><Link to='https://blog.graph.cool'>Github</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/HomeView/Faq/Faq.js
+++ src/components/HomeView/Faq/Faq.js
@@ -16,11 +16,23 @@ export default class Faq extends React.Component {
         </div>
         <div className={classes.questions}>
           <h3>Parse just shot down. Why should I bet on you?</h3>
-          <p>Good question! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>
+            Good question! Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
           <h3>How can I implement my server business logic?</h3>
-          <p>Using webhooks. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>
+            Using webhooks. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
           <h3>Can I import existing data from services like Parse?</h3>
-          <p>Yes. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>
+            Yes. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
         </div>
         <div className={classes.chat}>
           <Icon width={69} height={60} src={require('assets/icons/handshake.svg')} />
