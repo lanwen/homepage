bc7a89e067de8e88f43e18a2d8888c3e827afb15
Alicja <dul.alicja@gmail.com>
2016-11-25 15:39:13 +0100

    Changing styles to $p. when possible
    # Conflicts:
    #	package.json
    #	src/components/Header/Header.js
    #	static/_redirects
    #	webpack.config.build.js
    #	webpack.config.js
    #	yarn.lock

--- src/components/HomeView/Features/Features.tsx
+++ src/components/HomeView/Features/Features.tsx
@@ -39,9 +39,9 @@ export default class Features extends React.Component<{}, {}> {
 
     return (
       <div>
-      <div className={cx($p.pt96, $p.pb38, $p.tc, $p.f38, $p.fw3)}>
-        Features optimized for developer experience
-      </div>
+        <div className={cx($p.pt96, $p.pb38, $p.tc, $p.f38, $p.fw3)}>
+          Features optimized for developer experience
+        </div>
         <div className={cx($p.f20, $p.o50, $p.tc, $p.pb96)}>
           Graphcool is built by developers for developers.
         </div>
@@ -49,57 +49,57 @@ export default class Features extends React.Component<{}, {}> {
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Interactive Docs'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Interactive Docs'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Flexible Data Model'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Flexible Data Model'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Works with your stack'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Works with your stack'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='No vendor lock-in'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='No vendor lock-in'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Auto-scaling'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Auto-scaling'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Integrations'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Integrations'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Extensibility'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Extensibility'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
           <SmallContainer>
             <Icon />
             <FeaturesContent
-                headline='Zero maintenance'
-                content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
+              headline='Zero maintenance'
+              content='It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends.'
             />
           </SmallContainer>
         </Container>
--- src/components/HomeView/SchemaGraph.tsx
+++ src/components/HomeView/SchemaGraph.tsx
@@ -5,46 +5,46 @@ import styled from 'styled-components'
 import { breakpoints } from '../../utils/constants'
 
 const Root = styled.div`
-      display: flex;
-      flex-direction: column;
-      align-items: center; 
-      background-color: rgba(0, 0, 0, 0.03);
-      padding: 20px 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  background-color: rgba(0, 0, 0, 0.03);
+  padding: 20px 300px;
       
-      @media (max-width: ${breakpoints.p1200}px) {
-        padding: 50px 150px;
-        background-color: violet;
-      }
-      @media (max-width: ${breakpoints.p1000}px) {
-        padding: 50px 100px;
-        background-color: red;
-      }
-      @media (max-width: ${breakpoints.p750}px) {
-        padding: 25px 40px;
-        background-color: blue;
-      }
-      @media (max-width: ${breakpoints.p580}px) {
-        background-color: green;
-        padding: 15px 15px;
-      }
-      @media (max-width: ${breakpoints.p500}px) {
-        background-color: yellow;
-        padding: 10px 10px;
-      }
+    @media (max-width: ${breakpoints.p1200}px) {
+      padding: 50px 150px;
+      background-color: violet;
+    }
+    @media (max-width: ${breakpoints.p1000}px) {
+      padding: 50px 100px;
+      background-color: red;
+    }
+    @media (max-width: ${breakpoints.p750}px) {
+      padding: 25px 40px;
+      background-color: blue;
+    }
+    @media (max-width: ${breakpoints.p580}px) {
+      background-color: green;
+      padding: 15px 15px;
+    }
+    @media (max-width: ${breakpoints.p500}px) {
+      background-color: yellow;
+      padding: 10px 10px;
+    }
 `
 export default class SchemaGraph extends React.Component<{}, {}> {
 
-    render() {
-        return (
-            <Root>
-                <div className={cx($p.pt38, $p.pb38, $p.tc, $p.f38, $p.fw3)}>
-                    How it works
-                </div>
-                <div className={cx($p.f20, $p.o50, $p.tc, $p.pb16)}>
-                    I have hinted that I would often jerk poor Queequeg from between the whale and the ship where he would occasionally fall, from the incessant rolling and swaying of both.
-                </div>
-                <img className={cx($p.w100)} src={require('../../assets/graphics/architecture.svg')} alt='Architecture' />
-            </Root>
-        )
-    }
+  render() {
+    return (
+      <Root>
+        <div className={cx($p.pt38, $p.pb38, $p.tc, $p.f38, $p.fw3)}>
+          How it works
+        </div>
+        <div className={cx($p.f20, $p.o50, $p.tc, $p.pb16)}>
+          I have hinted that I would often jerk poor Queequeg from between the whale and the ship where he would occasionally fall, from the incessant rolling and swaying of both.
+        </div>
+        <img className={cx($p.w100)} src={require('../../assets/graphics/architecture.svg')} alt='Architecture'/>
+      </Root>
+    )
+  }
 }
