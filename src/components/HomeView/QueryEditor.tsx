775f472fd3931be140bd8bc0359e8987ffa18165
Julian Bauer <julian@overnice.de>
2016-11-29 11:46:58 +0100

    tabbar

--- src/components/HomeView/QueryEditor.tsx
+++ src/components/HomeView/QueryEditor.tsx
@@ -1,10 +1,55 @@
 import * as React from 'react'
 import * as cx from 'classnames'
-import { $p } from 'graphcool-styles'
+import { $p, $v } from 'graphcool-styles'
 import styled from 'styled-components'
 import SectionHeader from './SectionHeader'
 
 const Root = styled.div`
+  
+`
+
+const ActiveTab = `
+  padding: ${parseFloat($v.size16) + 2}px;
+  background: ${$v.darkBlue};
+  border-radius: 2px;
+  color: ${$v.white};
+  cursor: default;
+  
+  &:hover {
+    background: ${$v.darkBlue};
+    color: ${$v.white};
+  }
+`
+
+const Tab = styled.div`
+  color: ${$v.gray30};
+  letter-spacing: 1px;
+  font-size: ${$v.size16};
+  text-transform: uppercase;
+  font-weight: 600;
+  padding: ${$v.size16};
+  line-height: 1;
+  background: ${$v.gray07};
+  cursor: pointer;
+  
+  &:first-child {
+    border-top-left-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
+  
+  &:last-child {
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
+  
+  transition: background ${$v.duration} linear, color ${$v.duration} linear;
+  
+  &:hover {
+    background: ${$v.gray10};
+    color: ${$v.gray50};
+  }
+  
+  ${props => props.active && ActiveTab}
   
 `
 
@@ -18,7 +63,12 @@ export default class QueryEditor extends React.Component<{}, {}> {
           copy='I have hinted that I would often jerk poor Queequeg from between the whale and the ship where he would.'
         />
         <Root>
-
+          <div className={cx($p.flex, $p.relative, $p.itemsCenter, $p.justifyCenter)}>
+            <Tab>Instagram</Tab>
+            <Tab active>Twitter</Tab>
+            <Tab>To do list</Tab>
+            <Tab>Pokedex</Tab>
+          </div>
         </Root>
       </section>
     )
