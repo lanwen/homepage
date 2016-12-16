756702f69d0adf839905f49906e9f59af84dd53d
Julian <julian.tapales@gmail.com>
2016-12-17 00:04:38 +0100

    Clean up of content handler

--- src/components/DocsView/ContentHandler/ContentHandler.tsx
+++ src/components/DocsView/ContentHandler/ContentHandler.tsx
@@ -24,19 +24,23 @@ interface Item {
   body: string
 }
 
-export default class ContentHandler extends React.Component<Props, State> {
-  static contextTypes: React.ValidationMap<any> = {
-    router: React.PropTypes.func.isRequired,
-  }
+interface Context {
+  router: any
+}
 
-  state = {
-    ast: null,
+export default class ContentHandler extends React.Component<Props, State> {
+  context: Context
+  constructor(props) {
+    super(props)
+    this.state = {
+      ast: null,
+    }
   }
 
   componentDidMount() {
     const pathname = this.props.location.pathname.split('/')
     const contentAlias = pathname[pathname.length - 1]
-
+    const queryParams = {alias: contentAlias}
     const query = `
       query getItem($alias: String) {
         Item(alias: $alias) {
@@ -48,18 +52,13 @@ export default class ContentHandler extends React.Component<Props, State> {
       }
     `
 
-    const vars = {alias: contentAlias}
-
-    client.query(query, vars).then(result => {
+    client.query(query, queryParams).then(result => {
       if (result.Item) {
-        const parser = new Parser()
-        const parsedDecodedResult = parser.parse(decode64(result.Item.body))
+        const parsedDecodedResult = new Parser().parse(decode64(result.Item.body))
         this.setState({ast: parsedDecodedResult} as State)
       } else {
-        console.log('alias was invalid, redirecting...')
         this.context.router.push('/404')
       }
-      console.log(result)
     })
   }
   componentWillReceiveProps(nextProps) {
