f1874d9130a817994032a3506b39b4062d786049
Alicja <dul.alicja@gmail.com>
2016-12-16 15:51:57 +0100

    Related Content @media adjustments

--- src/components/DocsView/RelatedContent/RelatedContent.tsx
+++ src/components/DocsView/RelatedContent/RelatedContent.tsx
@@ -11,11 +11,16 @@ export default class RelatedContent extends React.Component<{}, {}> {
 
   render() {
     const Container = styled.div`
-      background: linear-gradient(to top, #fff 05%, rgba(0, 0, 0, 0.03) 15%,rgba(0, 0, 0, 0.03) 95%,#fff 85%);
+      background: linear-gradient(to top, #fff 13%, rgba(0, 0, 0, 0.03) 13%,rgba(0, 0, 0, 0.03) 95%,#fff 95%);
+      @media(max-width: ${breakpoints.p1000}px) {
+          background: linear-gradient(to top, #fff 5%, rgba(0, 0, 0, 0.03) 5%,rgba(0, 0, 0, 0.03) 95%,#fff 95%);
 `
     const Square = styled.div`
       box-shadow:0 8px 18px rgba(0, 0, 0, 0.03),
       0 -8px 18px rgba(0, 0, 0, 0.03);
+        
+        @media(max-width: ${breakpoints.p1000}px) {
+          margin: 0;
 `
     const Circle = styled.div`
       background-color: ${(props) => props.background};
@@ -24,10 +29,17 @@ export default class RelatedContent extends React.Component<{}, {}> {
       height: 20px;
 `
     const Rest = styled.div`
+     @media(max-width: ${breakpoints.p1000}px) {
+      margin-bottom: ${$v.size25};
+      margin-top: ${$v.size25};
+      
+      @media(max-width: ${breakpoints.p900}px) {
+        padding-left: 0;
 `
     return (
       <div>
-        <div className={cx($p.bBlack50, $p.bt, $p.mv38, $p.pv16, $p.flex, $p.justifyCenter)}>
+        <div className={cx($p.bBlack10, $p.mt60, $p.bt, $p.mh96)}/>
+        <div className={cx($p.mb38, $p.mt10, $p.pv16, $p.flex, $p.justifyCenter, $p.flexWrap)}>
           <p className={cx($p.o40, $p.f20, $p.fw4, $p.pt4)}>Was it helpful?</p>
           <img src={require('../../../assets/graphics/Yes.svg')}
                className={cx($p.bbox, $p.db, $p.pl38, $p.pr10)}
@@ -38,7 +50,7 @@ export default class RelatedContent extends React.Component<{}, {}> {
           />
           <p className={cx($p.o40, $p.f20, $p.fw4, $p.pt4)}>No</p>
         </div>
-        <Container className={cx($p.flex, $p.justifyCenter)}>
+        <Container className={cx($p.flex, $p.justifyCenter, $p.flexWrap, $p.ph10)}>
           <Square className={cx($p.flex, $p.flexColumn, $p.mb60, $p.pb25, $p.bgWhite, $p.mr25)}>
             <div className={cx($p.inlineFlex, $p.bgLightgreen10, $p.pa25)}>
               <p className={cx($p.lightgreen50, $p.pr10, $p.fw6, $p.f16)}>MORE ABOUT</p>
@@ -96,9 +108,9 @@ export default class RelatedContent extends React.Component<{}, {}> {
               </div>
             </div>
           </Square>
-          <Rest className={cx($p.mt96)}>
-            <p className={cx($p.fw6, $p.f16, $p.black30, $p.ph25, $p.pb25)}>FURTHER READING</p>
-            <div className={cx($p.flex, $p.ph25, $p.pv10)}>
+          <Rest className={cx($p.mt96, $p.pl25)}>
+            <p className={cx($p.fw6, $p.f16, $p.black30, $p.pb25)}>FURTHER READING</p>
+            <div className={cx($p.flex, $p.pv10)}>
               <Circle
                 background=''
                 className={cx($p.bbox, $p.db, $p.mr16, $p.mt4)}
@@ -114,7 +126,7 @@ export default class RelatedContent extends React.Component<{}, {}> {
                 <p className={cx($p.black30, $p.f14, $p.fw6)}>FAQ</p>
               </div>
             </div>
-            <div className={cx($p.flex, $p.ph25, $p.pv10)}>
+            <div className={cx($p.flex, $p.pv10)}>
               <Circle
                 background='rgba(39, 174, 96, 0.2)'
                 className={cx($p.bbox, $p.db, $p.mr16, $p.mt4)}
