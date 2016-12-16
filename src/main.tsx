15dd0c09c1ac0d350b901ce71e2340cb6b7852cc
Julian <julian.tapales@gmail.com>
2016-12-16 17:41:21 +0100

    Added stubs in component files and updated routes

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,11 @@ import FAQ from './components/DocsView/FAQ/FAQ'
 import * as FastClick from 'fastclick'
 import './style'
 import HeaderDocs from './components/DocsView/HeaderDocs'
+import QuickstartPage from './pages/quickstart/QuickstartPage'
+import ReferencePage from './pages/reference/ReferencePage'
+import ResourcesPage from './pages/resources/ResourcesPage'
+import BlogPage from './pages/blog/BlogPage'
+import CommunityPage from './pages/community/CommunityPage'
 
 function render() {
   ReactDOM.render(
@@ -20,11 +25,11 @@ function render() {
         <Route path='/about' component={AboutView}/>
         <Route path='/faq' component={FAQ}/>
         <Route path='/docs' component={DocsView}>
-          <Route path='quickstart' component={DocsView}/>
-          <Route path='resources' component={DocsView}/>
-          <Route path='reference' component={DocsView}/>
-          <Route path='blog' component={DocsView}/>
-          <Route path='community' component={DocsView}/>
+          <Route path='quickstart' component={QuickstartPage}/>
+          <Route path='resources' component={ResourcesPage}/>
+          <Route path='reference' component={ReferencePage}/>
+          <Route path='blog' component={BlogPage}/>
+          <Route path='community' component={CommunityPage}/>
         </Route>
       </Router>
     </AppContainer>,
---
+++ src/pages/quickstart/QuickstartPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class QuickstartPage extends React.Component<{}, {}> {
+
+  render() {
+
+    return (
+      <h1>Landing page for the quickstart page</h1>
+    )
+  }
+}
