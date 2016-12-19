340c60045f72bd1309778d5ec8085f57f2ad1f5d
Johannes Schickling <schickling.j@gmail.com>
2016-12-19 15:41:15 +0100

    added sidebar structure

---
+++ src/components/DocsView/components/reference-sidebar.ts
@@ -0,0 +1,18 @@
+interface Element {
+  alias: string
+  children?: Element[]
+}
+
+export const elements: Element[] = [{
+  alias: 'uh8shohxie',
+  children: [{
+    alias: 'ahwoh2fohj',
+  }, {
+    alias: 'teizeit5se',
+  }],
+}, {
+  alias: 'heshoov3ai',
+  children: [{
+    alias: 'wooghee1za',
+  }],
+}]
