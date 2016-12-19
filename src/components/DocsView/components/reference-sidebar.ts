384babbeaf60385b74ec01aca6cf7126b8891f72
Nilan <nilan.marktanner@student.kit.edu>
2016-12-19 16:21:35 +0100

    added side nav aliases

--- src/components/DocsView/components/reference-sidebar.ts
+++ src/components/DocsView/components/reference-sidebar.ts
@@ -4,15 +4,114 @@ interface Element {
 }
 
 export const elements: Element[] = [{
-  alias: 'uh8shohxie',
+  alias: 'chohbah0eo', //platform
   children: [{
-    alias: 'ahwoh2fohj',
+    alias: 'uh8shohxie', // console
+    children: [{
+      alias: 'he6jaicha8'
+    }, {
+      alias: 'ahlohd8ohn'
+    }, {
+      alias: 'iegoo0heez'
+    }]
   }, {
-    alias: 'teizeit5se',
+    alias: 'ahwoh2fohj', // data-schema
+    children: [{
+      alias: 'ij2choozae'
+    }, {
+      alias: 'teizeit5se'
+    }, {
+      alias: 'goh5uthoc1'
+    }, {
+      alias: 'uhieg2shio'
+    }]
+  }, {
+    alias: 'wejileech9' // authentication
+  }, {
+    alias: 'eer4wiang0' // file-management
   }],
 }, {
-  alias: 'heshoov3ai',
+  alias: 'heshoov3ai', // simple-api
   children: [{
-    alias: 'wooghee1za',
+    alias: 'koo4eevun4', // making-api-requests
+  }, {
+    alias: 'nia9nushae', // generated-queries
+    children: [{
+      alias: 'ua6eer7shu' // one node
+    }, {
+      alias: 'pa2aothaec', // multiple nodes
+      children: [{
+        alias: 'vequoog7hu' // ordering
+      }, {
+        alias: 'xookaexai0' // filtering
+      }, {
+        alias: 'ojie8dohju' // pagination
+      }]
+    }, {
+      alias: 'aihaeph5ip' // traversing
+    }, {
+      alias: 'gieh7iw2ru' // session-user
+    }]
+  }, {
+    alias: 'ol0yuoz6go', // generated-mutations
+    children: [{
+      alias: 'wooghee1za' // creating
+    }, {
+      alias: 'cahkav7nei' // updating
+    }, {
+      alias: 'fasie2rahv' // deleting
+    }, {
+      alias: 'zeich1raej' // one-to-one
+    }, {
+      alias: 'ofee7eseiy' // one-to-many
+    }, {
+      alias: 'eixu9osueb' // user-authentication
+    }, {
+      alias: 'eetai5meic' // file-management
+    }]
+  }, {
+    alias: 'aecou7haj9' //errors
+  }],
+}, {
+  alias: 'aizoong9ah', // relay-api
+  children: [{
+    alias: 'thaiph8ung', // making-api-requests
+  }, {
+    alias: 'oiviev0xi7', // generated-queries
+    children: [{
+      alias: 'ga4chied8m' // one node
+    }, {
+      alias: 'uu4ohnaih7', // multiple nodes
+      children: [{
+        alias: 'sa8aiwurae' // ordering
+      }, {
+        alias: 'aephaimu5n' // filtering
+      }, {
+        alias: 'riekooth4o' // pagination
+      }]
+    }, {
+      alias: 'uo6uv0ecoh' // traversing
+    }, {
+      alias: 'peyaaph9vi' // session-user
+    }]
+  }, {
+    alias: 'vah0igucil', // generated-mutations
+    children: [{
+      alias: 'oodoi6zeit' // creating
+    }, {
+      alias: 'uath8aifo6' // updating
+    }, {
+      alias: 'oojie3ooje' // deleting
+    }, {
+      alias: 'da7pu3seew' // one-to-one
+    }, {
+      alias: 'ek8eizeish' // one-to-many
+    }, {
+      alias: 'yoh9thaip0' // user-authentication
+    }, {
+      alias: 'aechiosh8u' // file-management
+    }]
+  }, {
+    alias: 'looxoo7avo' // errors
   }],
 }]
