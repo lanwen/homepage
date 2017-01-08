d4eb7f613113ff46c1a8776de60a151728bc3957
Nikolas Burk <nikolas.burk@gmail.com>
2017-01-08 21:03:31 +0100

    finished CallToAction

--- src/components/PricingView/CallToAction.tsx
+++ src/components/PricingView/CallToAction.tsx
@@ -11,23 +11,24 @@ const Box = styled.div`
 export default class CallToAction extends React.Component<{}, {}> {
   render() {
     return (
-      <div className={cx($p.flex, $p.bgWhite, $p.justifyAround)} style={{height: 180}}>
-        <Box className={cx($p.flex, $p.flexColumn, $p.pt60, $p.ph60, $p.bgrRed)}>
+
+
+        <Box className={cx($p.flex, $p.pv60, $p.ph60, $p.justifyBetween, $p.center)}>
           
           {/* FIRST COLUMN */}
-          <div className={cx()}>
-            <div>Ready to get started?</div>
-            <div>Setup your own GraphQL backend in less than 5 minutes.</div>
+          <div className={cx($p.flex, $p.flexColumn, $p.contentStart)}>
+            <div className={cx($p.green, $p.f25, $v.gray60)}>Ready to get started? </div>
+            <div>Setup your own GraphQL backend in less than 5 minutes.
+            </div>
           </div>
-
 
           {/* SECOND COLUMN */}
-          <div className={cx($p.flex, $p.flexColumn, $p.justifyEnd)}>
-            <div>className={cx($g.uppercaseButton, $p.bgGreen, $p.white)}>Sign up</div>
-            <div>className={cx($g.uppercaseButton, $p.bgGreen, $p.white)}>Talk to sales</div>
+          <div className={cx($p.flex, $p.pv6, $p.itemsCenter)}>
+            <div className={cx($g.uppercaseButton, $p.bgGreen, $p.white, $p.tc)}>Sign up</div>
+            <div className={cx($g.uppercaseButton, $p.bgWhite, $p.green, $p.tc, $p.ba, $p.bGreen, $p.ml10)}>Talk to sales</div>
           </div>
+
         </Box>
-      </div>
     )
   }
 }
