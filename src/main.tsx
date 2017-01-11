47e0d60caf5c2d1ba725092392ad91fa9029cde0
Tim Suchanek <tim.suchanek@gmail.com>
2017-01-11 15:59:45 +0100

    fix docs route

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react' // tslint:disable-line
 import * as ReactDOM from 'react-dom'
-import { Router, Route, browserHistory, applyRouterMiddleware } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory, applyRouterMiddleware } from 'react-router'
 import { useScroll } from 'react-router-scroll'
 import { AppContainer } from 'react-hot-loader'
 import HomeView from './components/HomeView/HomeView'
@@ -44,7 +44,7 @@ function render() {
             <Route path='/pricing' component={PricingView}/>
             <Route path='/about' component={AboutView}/>
             <Route path='/docs' component={DocsView}>
-              <Route path='overview' component={DocsOverview}/>
+              <IndexRoute component={DocsOverview}/>
               <Route path='quickstart' component={QuickstartPage}/>
               <Route path='resources' component={ResourcesPage}/>
               <Route path='reference' component={ReferencePage}/>
