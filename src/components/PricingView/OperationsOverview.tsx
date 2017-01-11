fde95467f4f0db52951c7376935bd4ebee8659df
Johannes Schickling <schickling.j@gmail.com>
2017-01-11 17:22:34 +0100

    Refactornig

--- src/components/PricingView/OperationsOverview.tsx
+++ src/components/PricingView/OperationsOverview.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { $p, $v, $g, Icon } from 'graphcool-styles'
 import * as cx from 'classnames'
-import OperationSlider from './OperationSlider'
+import OperationSliderCard from './OperationSliderCard'
 import styled from 'styled-components'
+import { numberWithCommas, roundedStep } from '../../utils/pricing'
 
 const Circle36 = styled.div`
   width: 36px;
@@ -10,7 +11,6 @@ const Circle36 = styled.div`
 `
 
 interface State {
-  totalOperations: number
   leftValue1: number
   rightValue1: number
   leftValue2: number
@@ -21,132 +21,92 @@ interface State {
 
 export default class OperationOverview extends React.Component<{}, State> {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      totalOperations: 0,
-        leftValue1: 0,
-        rightValue1: 0,
-        leftValue2: 0,
-        rightValue2: 0,
-        leftValue3: 0,
-        rightValue3: 0,
-    }
+  state = {
+    leftValue1: 0,
+    rightValue1: 1,
+    leftValue2: 0,
+    rightValue2: 1,
+    leftValue3: 0,
+    rightValue3: 3,
   }
 
   render() {
-
-    const {totalOperations} = this.state
+    const totalOperations = roundedStep(this.state.leftValue1) * roundedStep(this.state.rightValue1)
+      + roundedStep(this.state.leftValue2) * roundedStep(this.state.rightValue2)
+      + roundedStep(this.state.leftValue3) * roundedStep(this.state.rightValue3)
 
     return (
-        <div className={cx($p.flex, $p.flex1, $p.flexColumn, $p.pl60)}>
-          <div className={cx($p.flex, $p.justifyCenter, $p.itemsCenter)}>
-            <div>
-              <Circle36 className={cx($p.bgWhite20, $p.br100, $p.flex, $p.justifyCenter, $p.itemsCenter)}>
-                <Icon
-                  src={require('../../assets/icons/pricing/operation_white.svg')}
-                  color={$v.white}
-                  height={17}
-                  width={11}
-                />
-              </Circle36>
-            </div>
-            <div className={cx($p.white, $p.ml16)}>What is an operation?</div>
+      <div className={cx($p.flex, $p.flex1, $p.flexColumn, $p.pl60)}>
+        <div className={cx($p.flex, $p.justifyCenter, $p.itemsCenter)}>
+          <div>
+            <Circle36 className={cx($p.bgWhite20, $p.br100, $p.flex, $p.justifyCenter, $p.itemsCenter)}>
+              <Icon
+                src={require('../../assets/icons/pricing/operation_white.svg')}
+                color={$v.white}
+                height={17}
+                width={11}
+              />
+            </Circle36>
           </div>
-          <div className={cx($p.white80, $p.f14, $p.fw3, $p.tc, $p.pt4, $p.mt25, $p.mb38)}>
-                There are three operation types: requests,
-                subscriptions & function calls
-          </div>
-
-          {/* Card 1: Requests */}
-          {/* should be X leaf nodes _per request_ */}
-          <OperationSlider
-            operationType='Request'
-            description='A request is either a GraphQL query or mutation.'
-            leftInfo='requests/month'
-            rightInfo='leaf nodes'
-            leftSliderMaxValue={19}
-            rightSliderMaxValue={16}
-            leftLink=''
-            rightLink='a'
-            leftValue={this.state.leftValue1 === 0 ? 0 : this.state.leftValue1 + 9}
-            rightValue={this.state.rightValue1}
-            onLeftSliderValueChange={(value) => this.onSliderChanged(0, value)}
-            onRightSliderValueChange={(value) => this.onSliderChanged(1, value)}
-           />
-
-          {/* Card 2: Subscriptions */}
-          <OperationSlider
-            operationType='Subscription'
-            description='A GraphQL subscription is triggered by a mutation.'
-            leftInfo='mutations/month'
-            rightInfo='concurrent subscriptions'
-            leftSliderMaxValue={19}
-            rightSliderMaxValue={19}
-            leftLink=''
-            rightLink='a'
-            leftValue={this.state.leftValue2 === 0 ? 0 : this.state.leftValue2 + 9}
-            rightValue={this.state.rightValue2}
-            onLeftSliderValueChange={(value) => this.onSliderChanged(2, value)}
-            onRightSliderValueChange={(value) => this.onSliderChanged(3, value)}
-          />
-
-          {/* Card 3: Functions */}
-          <OperationSlider
-            operationType='Function calls'
-            description='Function calls are billed by execution time.'
-            leftInfo='function calls/month'
-            rightInfo='ms per function call'
-            leftSliderMaxValue={19}
-            rightSliderMaxValue={16}
-            leftLink=''
-            leftValue={this.state.leftValue3 === 0 ? 0 : this.state.leftValue3 + 9}
-            rightValue={this.state.rightValue3}
-            onLeftSliderValueChange={(value) => this.onSliderChanged(4, value)}
-            onRightSliderValueChange={(value) => this.onSliderChanged(5, value)}
-           />
-
-          <span className={cx($p.white, $p.f16, $p.tr, $p.mt4)}>
-            <b>= {this.numberWithCommas(this.state.totalOperations)}</b> operations/month
-          </span>
+          <div className={cx($p.white, $p.ml16)}>What is an operation?</div>
         </div>
+        <div className={cx($p.white80, $p.f14, $p.fw3, $p.tc, $p.pt4, $p.mt25, $p.mb38)}>
+          There are three operation types: requests,
+          subscriptions & function calls
+        </div>
+
+        {/* Card 1: Requests */}
+        {/* should be X leaf nodes _per request_ */}
+        <OperationSliderCard
+          operationType='Request'
+          description='A request is either a GraphQL query or mutation.'
+          leftInfo='requests/month'
+          rightInfo='leaf nodes per request'
+          leftSliderMaxValue={19}
+          rightSliderMaxValue={16}
+          leftLink=''
+          rightLink='a'
+          leftValue={this.state.leftValue1 === 0 ? 0 : this.state.leftValue1 + 9}
+          rightValue={this.state.rightValue1}
+          onLeftSliderValueChange={(leftValue1) => this.setState({ leftValue1 } as State)}
+          onRightSliderValueChange={(rightValue1) => this.setState({ rightValue1 } as State)}
+        />
+
+        {/* Card 2: Subscriptions */}
+        <OperationSliderCard
+          operationType='Subscription'
+          description='A GraphQL subscription is triggered by a mutation.'
+          leftInfo='mutations/month'
+          rightInfo='concurrent subscriptions'
+          leftSliderMaxValue={19}
+          rightSliderMaxValue={19}
+          leftLink=''
+          rightLink='a'
+          leftValue={this.state.leftValue2 === 0 ? 0 : this.state.leftValue2 + 9}
+          rightValue={this.state.rightValue2}
+          onLeftSliderValueChange={(leftValue2) => this.setState({ leftValue2 } as State)}
+          onRightSliderValueChange={(rightValue2) => this.setState({ rightValue2 } as State)}
+        />
+
+        {/* Card 3: Functions */}
+        <OperationSliderCard
+          operationType='Function calls'
+          description='Function calls are billed by execution time.'
+          leftInfo='function calls/month'
+          rightInfo='ms per function call'
+          leftSliderMaxValue={19}
+          rightSliderMaxValue={16}
+          leftLink=''
+          leftValue={this.state.leftValue3 === 0 ? 0 : this.state.leftValue3 + 9}
+          rightValue={this.state.rightValue3}
+          onLeftSliderValueChange={(leftValue3) => this.setState({ leftValue3 } as State)}
+          onRightSliderValueChange={(rightValue3) => this.setState({ rightValue3 } as State)}
+        />
+
+        <span className={cx($p.white, $p.f16, $p.tr, $p.mt4)}>
+          <b>= {numberWithCommas(totalOperations)}</b> operations/month
+        </span>
+      </div>
     )
-  }
-
-  private numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  }
-
-  private onSliderChanged = (sliderIndex: number, value: number) => {
-    switch (sliderIndex) {
-    case 0:
-      this.setState({leftValue1: value} as State)
-      break
-    case 1:
-      this.setState({rightValue1: value} as State)
-      break
-    case 2:
-      this.setState({leftValue2: value} as State)
-      break
-    case 3:
-      this.setState({rightValue2: value} as State)
-      break
-    case 4:
-      this.setState({leftValue3: value} as State)
-      break
-    case 5:
-      this.setState({rightValue3: value} as State)
-      break
-    default:
-      break
-    }
-    this.updateTotalOperations()
-  }
-
-  private updateTotalOperations () {
-    const result = this.state.leftValue1 + this.state.rightValue1
-                 + this.state.leftValue2 + this.state.rightValue2
-                 + this.state.leftValue3 + this.state.rightValue3
-    this.setState({totalOperations: result} as State)
   }
 }
