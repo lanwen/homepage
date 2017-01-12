5bccdb2e3651f34cb95e57c82eab1dd5841577a3
Tim Suchanek <tim.suchanek@gmail.com>
2017-01-12 15:16:38 +0100

    implement blog page

--- src/components/DocsView/pages/BlogPage/BlogPage.tsx
+++ src/components/DocsView/pages/BlogPage/BlogPage.tsx
@@ -1,38 +1,47 @@
 import * as React from 'react'
-import { graphql } from 'react-apollo'
-import { withRouter } from 'react-router'
-import TemporaryNavigation from '../../components/TemporaryNavigation'
-import { getItemsByLayout } from '../../fragments/getItemsByLayout'
-import AnnoucementBox from './AnnouncementBox'
-import { $p, $v } from 'graphcool-styles'
+import {$p} from 'graphcool-styles'
 import * as cx from 'classnames'
 import styled from 'styled-components'
-import BlogPost from './BlogPost'
+import Items from '../Overview/Items'
+import Heading from '../Overview/Heading'
 
-interface Props {
-  data: any
-}
-const Basis = styled.div`
-  margin-top: 200px;
+const Container = styled.div`
+  max-width: 1050px;
+  margin: 0 auto;
 `
-class BlogPage extends React.Component<Props, {}> {
+
+const ContentContainer = styled.div`
+   flex: 1 1 100px;
+`
+
+const ItemsBackground = styled.div`
+  background: rgb(250,250,250);
+`
+
+const StyledItems = styled(Items)`
+  top: -10px;
+`
+
+export default class TutorialsPage extends React.Component<{}, {}> {
+
   render() {
     return (
-      <Basis className={cx($p.ph96, $p.flex, $p.flexColumn, $p.flexWrap)}>
-        {this.props.data.loading ? 'loading...' : <TemporaryNavigation links={this.props.data.allItems}/>}
-        <AnnoucementBox/>
-        <BlogPost/>
-      </Basis>
+      <ContentContainer className={cx($p.pt96, $p.mt96)}>
+        <Container>
+          <Heading
+            layout='BLOG'
+            title='The Graphcool Blog'
+            text='The latest news around Graphcool & GraphQL'
+            link='/docs/blog'
+          />
+        </Container>
+        <ItemsBackground className={$p.mt60}>
+          <Container>
+            <StyledItems count={50} layout='BLOG' className={cx($p.relative)} showPreview={false} />
+          </Container>
+        </ItemsBackground>
+      </ContentContainer>
     )
   }
 }
 
-const BlogPageWithData = graphql(getItemsByLayout, {
-  options: {
-    variables: {
-      layout: 'BLOG',
-    },
-  },
-})(BlogPage)
-
-export default withRouter(BlogPageWithData)
