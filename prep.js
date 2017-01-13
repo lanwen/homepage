d7ef61e2d1a0a1228d7111604ed6f0c170d70f04
Johannes Schickling <schickling.j@gmail.com>
2017-01-13 14:37:56 +0100

    Prerendering prep

--- prep.js
+++ prep.js
@@ -1,14 +1,33 @@
-exports.default = {
-  "routes": [
-    "/",
-    "/docs",
-    "/docs/faq",
-    "/docs/tutorials",
-    "/docs/quickstart",
-    "/about",
-    "/pricing",
-  ],
-  dimensions: {
-    width: 1240,
-  },
+const Lokka = require('lokka').Lokka
+const Transport = require('lokka-transport-http').Transport
+
+exports.default = () => {
+  const client = new Lokka({
+    transport: new Transport('https://api.graph.cool/simple/v1/ciwkuhq2s0dbf0131rcb3isiq')
+  })
+
+  return client.query(`{
+    allItems {
+      alias
+      path
+    }
+  }`).then(result => {
+    const docsItemRoutes = result.allItems.map(({alias, path}) => `${path}-${alias}`)
+    const routes = [
+      '/',
+      '/docs',
+      '/docs/faq',
+      '/docs/tutorials',
+      '/docs/quickstart',
+      '/about',
+      '/pricing',
+    ]
+
+    return {
+      routes: routes.concat(docsItemRoutes)
+      https: true,
+      hostname: 'https://www.graph.cool',
+    }
+  })
+
 }
