09738c5905ca7f8b3fb5733362f6a5c2086a6e34
Nikolas Burk <nikolas.burk@gmail.com>
2017-03-13 17:09:45 +0100

    added FAQs

--- src/components/GraphQLUpView/GraphQLUpFAQ.tsx
+++ src/components/GraphQLUpView/GraphQLUpFAQ.tsx
@@ -4,7 +4,7 @@ import {breakpoints} from '../../utils/constants'
 
 export interface FAQItem {
   question: string
-  answer: string
+  answer: JSX.Element
   readMoreLink?: string
 }
 
@@ -13,37 +13,38 @@ interface Props {
 
 const leftFAQItems: [FAQItem] = [
   {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
+    question: 'Do I have to create a Graphcool account in order to use graphql-up?',
+    answer: (<span>No, graphql-up is available for absolutely everyone interested in GraphQL.
+    You don't have to create an account or leave your email address. 
+    Of course, it's also completely free of charge.</span>),
   },
   {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
-  },
-  {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
+    question: 'How long can I use the GraphQL API that I created with graphql-up?',
+    answer: (<span>Since projects created with graphql-up are not intended for
+    production use, they will be deleted after 30 days of inactivity. 
+    If you're looking to build something more 
+    sustainable, consider creating a project with Graphcool.</span>),
   },
 ]
 
 const rightFAQItems: [FAQItem] = [
   {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
+    question: 'What does the generated GraphQL API look like?',
+    answer: (<span>graphql-up generates two different APIs. The first one is
+    called <a href='https://www.graph.cool/docs/reference/simple-api/overview-heshoov3ai'>Simple API</a> and
+    follows best practices of the Graphcool platform.
+    It can be used with any GraphQL client, such as Apollo or Lokka. 
+    The second is
+    a <a href='https://www.graph.cool/docs/reference/relay-api/overview-aizoong9ah/'>Relay API</a> for
+    dedicated use of Relay in the client.</span>),
   },
   {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
-  },
-  {
-    question: 'What happens when I go over my limit?',
-    answer: `Each plan has a over-quota price for additional nodes and requests. When you
-    start exceeding your plan we will let you know that it is time to change your plan.`,
+    question: 'What use cases does graphql-up cover?',
+    answer: (<span>graphql-up is ideal for anyone who wants to have a head-start with
+    GraphQL without the hazzle of having to do any work on the server. You
+    can use it to quickly spin up a GraphQL backend for mocking data, testing or simply
+    experimenting and playing around with GraphQL.
+    It's also perfect for usage in tutorials, sample projects or documentation.</span>),
   },
 ]
 
