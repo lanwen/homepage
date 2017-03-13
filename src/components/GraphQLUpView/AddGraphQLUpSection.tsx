666620552186bc781fad33415c43e5f432a4b60b
Tim Suchanek <tim.suchanek@gmail.com>
2017-03-13 21:35:23 +0100

    implement new add badge design

--- src/components/GraphQLUpView/AddGraphQLUpSection.tsx
+++ src/components/GraphQLUpView/AddGraphQLUpSection.tsx
@@ -51,87 +51,102 @@ export default class AddGraphQLUpSection extends React.Component<Props, State> {
 
   render() {
     return (
-      <div className='root'>
+      <div className='add-graphql-root'>
         <style jsx={true}>{`
-          .root {
-            @p: .flex, .flexColumn, .itemsCenter, .ph38, .center, .bgGreen10;
-            max-width: 1440px;
-          }
+            .add-graphql-root {
+              @p: .w100, .bgDarkBlue;
+            }
+            .add-graphql {
+              @p: .flex, .flexColumn, .itemsCenter, .ph38, .center, .white;
+              max-width: 1440px;
+            }
 
-          .innerMaxWidth {
-            max-width: 725px;
-          }
+            .innerMaxWidth {
+              max-width: 750px;
+            }
 
-          .title {
-            @p: .f25, .fw3, .pt60;
-          }
+            .title {
+              @p: .fw3, .pt60;
+              font-size: 31.5px;
+            }
 
-          .subtitle {
-            @p: .black60, .f20, .fw3, .pt16, .tc, .pb60;
-          }
+            .subtitle {
+              @p: .f20, .fw3, .pt16, .tc, .pb60, .white60;
+            }
 
-          .stepInstruction {
-            @p: .f14, .fw6, .ttu, .pl16;
-            color: rgba(39,174,96,0.5);
-          }
+            .stepInstruction {
+              @p: .f14, .fw6, .ttu, .pl16, .white;
+              letter-spacing: 0.53px;
+            }
 
-          .input {
-            @p: .w100, .ph25, .pv16, .bgWhite, .f14, .fw3, .mt10, .bbox;
-            min-height: 60px;
-          }
+            .input {
+              @p: .w100, .ph25, .pv16, .bgDarkerBlue, .f14, .fw3, .mt10, .bbox, .white;
+              min-height: 60px;
+            }
 
-          .copyMarkdown {
-            @p: .flex, .itemsCenter, .justifyBetween, .black60;
-          }
+            .copyMarkdown {
+              @p: .flex, .itemsCenter, .justifyBetween;
+            }
 
-        `}</style>
-        <div className='title innerMaxWidth flex'>
-          Add
-          <img
-            className='mh10'
-            src={require('../../assets/graphics/graphqlup/graphql-up_small.svg')}
-          />
-          to your repositories/docs {booksEmoji}
-          </div>
-        <div className='subtitle innerMaxWidth'>
-          You can add a graphql-up badge to your own repositories or docs to give your users a
-          quick way to get their own GraphQL endpoint based on your schema.
-        </div>
-
-        <div className='w100 pb60 innerMaxWidth'>
-          <div className='stepInstruction'>Step 1: Paste URL to your schema</div>
-          <input
-            className='input'
-            placeholder='Paste your URL here ...'
-            value={this.state.schemaUrl}
-            onChange={(e: any) => this.update(e.target.value)}
-          />
-
-          <div className='stepInstruction pt25'>Step 2: Copy generated markdown</div>
-          <div className='input copyMarkdown'>
-            <div>{this.state.markdown}</div>
-            <CopyToClipboard text={this.state.markdown} onCopy={this.onCopy}>
-              <div
-                className='pointer buttonShadow itemsCenter'
-              >
-                {this.state.copied &&
-                <CopyIndicator className='absolute f14 fw6 ml16'>
-                  Copied
-                </CopyIndicator>
-                }
-                <Icon
-                  className=''
-                  width={38}
-                  height={38}
-                  color={$v.darkerBlue}
-                  src={require('graphcool-styles/icons/fill/copy.svg')}
-                />
-              </div>
-            </CopyToClipboard>
+            .copyButton {
+              @p: .pointer, .buttonShadow, .itemsCenter, .bgWhite, .br2, .ml25;
+            }
+          `}</style>
+        <div className='add-graphql'>
+          <div className='title innerMaxWidth flex'>
+            Add
+            <a
+              href={'https://www.graph.cool/graphql-up/new?source=' +
+               'https://raw.githubusercontent.com/nikolasburk/ConferencePlanner/master/conference_planner.schema'}
+              target='_blank'
+            >
+              <img
+                className='mh10'
+                src={require('../../assets/graphics/graphqlup/graphql-up_small.svg')}
+              />
+            </a>
+            to your repositories/docs {booksEmoji}
             </div>
+          <div className='subtitle innerMaxWidth'>
+            You can add a graphql-up badge to your own repositories or docs to give your users a
+            quick way to get their own GraphQL endpoint based on your schema.
+          </div>
+
+          <div className='w100 pb60 innerMaxWidth'>
+            <div className='stepInstruction'>Step 1: Paste URL to your schema</div>
+            <input
+              className='input'
+              placeholder='Paste your URL here ...'
+              value={this.state.schemaUrl}
+              onChange={(e: any) => this.update(e.target.value)}
+            />
+
+            <div className='stepInstruction pt25'>Step 2: Copy generated markdown</div>
+            <div className='input copyMarkdown'>
+              <div>{this.state.markdown}</div>
+              <CopyToClipboard text={this.state.markdown} onCopy={this.onCopy}>
+                <div
+                  className='copyButton'
+                >
+                  {this.state.copied &&
+                  <CopyIndicator className='absolute f14 fw6 ml16'>
+                    Copied
+                  </CopyIndicator>
+                  }
+                  <Icon
+                    className=''
+                    width={38}
+                    height={38}
+                    color={$v.darkerBlue}
+                    src={require('graphcool-styles/icons/fill/copy.svg')}
+                  />
+                </div>
+              </CopyToClipboard>
+              </div>
+
+          </div>
 
         </div>
-
       </div>
     )
   }
