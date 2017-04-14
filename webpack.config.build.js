e8bf7fed5b603e7f777b62fa58f62dea0cce925f
Tim Suchanek <tim.suchanek@gmail.com>
2017-04-14 16:40:22 +0200

    build

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -50,10 +50,7 @@ module.exports = {
       loader: 'raw-loader!svgo-loader',
     }, {
       test: /(graphics|gifs)\/.*\.(svg|png|gif|jpg)$/,
-      loaders: [
-        'file-loader',
-        'image-webpack-loader&bypassOnDebug',
-      ],
+      loader: 'file-loader!image-webpack-loader&bypassOnDebug',
     }, {
       test: /\.md$/,
       loader: 'raw-loader',
