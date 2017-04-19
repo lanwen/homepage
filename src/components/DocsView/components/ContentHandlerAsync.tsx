887e853382a12332104f71888908f2fed1804a52
Tim Suchanek <tim.suchanek@gmail.com>
2017-04-19 18:31:32 +0200

    fix comments & braces in cm

--- src/components/DocsView/components/ContentHandlerAsync.tsx
+++ src/components/DocsView/components/ContentHandlerAsync.tsx
@@ -22,7 +22,6 @@ import ContentPagination from './ContentPagination'
 import {elements} from './ReferenceSidenav/data'
 import {extractAliases} from './ReferenceSidenav/ReferenceSidenav'
 import LoadingArticle from './LoadingArticle'
-import * as cookiestore from 'cookiestore'
 import {omit, flatMap, orderBy} from 'lodash'
 
 interface Props {
